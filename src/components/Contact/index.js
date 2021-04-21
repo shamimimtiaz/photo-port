1376cce03a94c59cea1a6b58bd1bda3d63732bf5
unknown <km_si@ymail.com>
2021-04-21 13:35:57 -0400

    create a contact form

---
+++ src/components/Contact/index.js
@@ -0,0 +1,34 @@
+
+
+function ContactForm() {
+
+    // JSX
+    }
+
+return (
+ <section>
+    <h1>Contact me</h1>
+    <form id="contact-form">
+    {/* name input
+        email input
+        message text area 
+        we need to replace the for attribute in the <label> element to htmlFor, 
+        */}
+
+    <div>  
+        <label htmlFor="name">Name:</label>
+        <input type="text" name="name" />
+    </div>
+    <div>
+        <label htmlFor="email">Email address:</label>
+        <input type="email" name="email" />
+    </div>
+    <div>
+        <label htmlFor="message">Message:</label>
+        <textarea name="message" rows="5"  />
+    </div>
+    <button type="submit">Submit</button>
+   </form>
+</section>
+      )
+    export default ContactForm;
